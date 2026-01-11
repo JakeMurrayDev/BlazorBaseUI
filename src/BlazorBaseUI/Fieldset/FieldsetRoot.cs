2eef8fbd77c4278c0a37215c748a7a18f8bd0620
jamurraydeveloper <jamurraydeveloper@gmail.com>
2026-01-12 00:09:21 +0800

    Refactor fieldset component for consistency

--- src/BlazorBaseUI/Fieldset/FieldsetRoot.cs
+++ src/BlazorBaseUI/Fieldset/FieldsetRoot.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
@@ -9,6 +8,8 @@ public sealed class FieldsetRoot : ComponentBase
     private const string DefaultTag = "fieldset";
 
     private string? legendId;
+    private FieldsetRootState state = new(Disabled: false);
+    private bool isComponentRenderAs;
 
     [Parameter]
     public bool Disabled { get; set; }
@@ -31,12 +32,25 @@ public sealed class FieldsetRoot : ComponentBase
     [Parameter(CaptureUnmatchedValues = true)]
     public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
 
-    [DisallowNull]
     public ElementReference? Element { get; private set; }
+
+    protected override void OnParametersSet()
+    {
+        isComponentRenderAs = RenderAs is not null;
+
+        if (isComponentRenderAs && !typeof(IReferencableComponent).IsAssignableFrom(RenderAs))
+        {
+            throw new InvalidOperationException($"Type {RenderAs!.Name} must implement IReferencableComponent.");
+        }
+
+        if (state.Disabled != Disabled)
+        {
+            state = new FieldsetRootState(Disabled);
+        }
+    }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        var state = new FieldsetRootState(Disabled);
         var context = new FieldsetRootContext(
             LegendId: legendId,
             SetLegendId: SetLegendId,
@@ -45,60 +59,57 @@ public sealed class FieldsetRoot : ComponentBase
         var resolvedClass = AttributeUtilities.CombineClassNames(AdditionalAttributes, ClassValue?.Invoke(state));
         var resolvedStyle = AttributeUtilities.CombineStyles(AdditionalAttributes, StyleValue?.Invoke(state));
 
-        var attributes = BuildAttributes(state);
-        if (!string.IsNullOrEmpty(resolvedClass))
-            attributes["class"] = resolvedClass;
-        if (!string.IsNullOrEmpty(resolvedStyle))
-            attributes["style"] = resolvedStyle;
-
         builder.OpenComponent<CascadingValue<FieldsetRootContext>>(0);
         builder.AddComponentParameter(1, "Value", context);
         builder.AddComponentParameter(2, "IsFixed", false);
         builder.AddComponentParameter(3, "ChildContent", (RenderFragment)(contentBuilder =>
         {
-            if (RenderAs is not null)
+            if (isComponentRenderAs)
             {
-                contentBuilder.OpenComponent(4, RenderAs);
-                contentBuilder.AddMultipleAttributes(5, attributes);
-                contentBuilder.AddComponentParameter(6, "ChildContent", ChildContent);
+                contentBuilder.OpenComponent(0, RenderAs!);
+                contentBuilder.AddMultipleAttributes(1, AdditionalAttributes);
+                if (!string.IsNullOrEmpty(legendId))
+                {
+                    contentBuilder.AddAttribute(2, "aria-labelledby", legendId);
+                }
+                if (Disabled)
+                {
+                    contentBuilder.AddAttribute(3, "disabled", true);
+                    contentBuilder.AddAttribute(4, "data-disabled", "");
+                }
+                contentBuilder.AddAttribute(5, "class", resolvedClass);
+                contentBuilder.AddAttribute(6, "style", resolvedStyle);
+                contentBuilder.AddComponentParameter(7, "ChildContent", ChildContent);
+                contentBuilder.AddComponentReferenceCapture(8, component => Element = ((IReferencableComponent)component).Element);
                 contentBuilder.CloseComponent();
             }
             else
             {
-                var tag = !string.IsNullOrEmpty(As) ? As : DefaultTag;
-                contentBuilder.OpenElement(7, tag);
-                contentBuilder.AddMultipleAttributes(8, attributes);
-                contentBuilder.AddElementReferenceCapture(9, e => Element = e);
-                contentBuilder.AddContent(10, ChildContent);
+                contentBuilder.OpenElement(9, !string.IsNullOrEmpty(As) ? As : DefaultTag);
+                contentBuilder.AddMultipleAttributes(10, AdditionalAttributes);
+                if (!string.IsNullOrEmpty(legendId))
+                {
+                    contentBuilder.AddAttribute(11, "aria-labelledby", legendId);
+                }
+                if (Disabled)
+                {
+                    contentBuilder.AddAttribute(12, "disabled", true);
+                    contentBuilder.AddAttribute(13, "data-disabled", "");
+                }
+                if (!string.IsNullOrEmpty(resolvedClass))
+                {
+                    contentBuilder.AddAttribute(14, "class", resolvedClass);
+                }
+                if (!string.IsNullOrEmpty(resolvedStyle))
+                {
+                    contentBuilder.AddAttribute(15, "style", resolvedStyle);
+                }
+                contentBuilder.AddElementReferenceCapture(16, elementReference => Element = elementReference);
+                contentBuilder.AddContent(17, ChildContent);
                 contentBuilder.CloseElement();
             }
         }));
         builder.CloseComponent();
-    }
-
-    private Dictionary<string, object> BuildAttributes(FieldsetRootState state)
-    {
-        var attributes = new Dictionary<string, object>();
-
-        if (AdditionalAttributes is not null)
-        {
-            foreach (var attr in AdditionalAttributes)
-            {
-                if (attr.Key is not "class" and not "style")
-                    attributes[attr.Key] = attr.Value;
-            }
-        }
-
-        if (!string.IsNullOrEmpty(legendId))
-            attributes["aria-labelledby"] = legendId;
-
-        if (Disabled)
-            attributes["Disabled"] = true;
-
-        foreach (var dataAttr in state.GetDataAttributes())
-            attributes[dataAttr.Key] = dataAttr.Value;
-
-        return attributes;
     }
 
     private void SetLegendId(string? id)
--- src/BlazorBaseUI/Fieldset/FieldsetRootState.cs
+++ src/BlazorBaseUI/Fieldset/FieldsetRootState.cs
@@ -1,12 +1,3 @@
 namespace BlazorBaseUI.Fieldset;
 
-public sealed record FieldsetRootState(bool Disabled)
-{
-    public static FieldsetRootState Default { get; } = new(Disabled: false);
-
-    public IEnumerable<KeyValuePair<string, object>> GetDataAttributes()
-    {
-        if (Disabled)
-            yield return new KeyValuePair<string, object>("data-Disabled", "");
-    }
-}
+public sealed record FieldsetRootState(bool Disabled);
